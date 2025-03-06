0ed3c799a79ba32c5acb30712b2d5b32ed35256a
Dhrubajyoti Bhattacharjee <dhrubajyotibhattacharjee182@gmail.com>
2025-03-06 16:53:24 +0530

    final commit

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,19 +1,6 @@
 const getCurrentYear = () => new Date().getFullYear();
 
 /**
- * Generates a unique alphanumeric ID.
- *
- * This function creates a random string using `Math.random()`,
- * converts it to a base-36 string, removes the leading "0.",
- * and appends the current timestamp in base-36 format to ensure uniqueness.
- *
- * @returns {string} A unique identifier.
- */
-const generateId = () => {
-  return Math.random().toString(36).slice(2) + Date.now().toString(36);
-};
-
-/**
  * Converts a given string to title case, where the first letter of each word is capitalized.
  *
  * @param {string} str - The input string to be converted.
